6641f02efe6ab9201ce3a7d8ecb448882a2df6ef
chriskryx <tanulchristian@live.com>
2022-05-02 10:15:42 +0300

    Simplify RequestsListView and context obj name in template

--- purchase_requests/requestform/views.py
+++ purchase_requests/requestform/views.py
@@ -1,5 +1,4 @@
 from django.urls import reverse_lazy
-from django.utils import timezone
 from django.views.generic import DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
@@ -8,13 +7,8 @@ from .models import Request
 
 
 class RequestsListView(ListView):
-
     model = Request
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["now"] = timezone.now()
-        return context
+    context_object_name = "requests"
 
 
 class RequestCreateView(CreateView):
