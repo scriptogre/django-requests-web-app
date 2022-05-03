15200635bacd32c470199e1015b59b401f8d58d9
chriskryx <tanulchristian@live.com>
2022-05-03 13:02:27 +0300

    Redesign Dashboard page, add extra context to DashboardView & implement dynamic variables to Dashboard page

--- purchase_requests/dashboard/views.py
+++ purchase_requests/dashboard/views.py
@@ -10,4 +10,22 @@ class DashboardView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["latest_requests"] = Request.objects.order_by("-id")[:5:-1]
         context["total_requests"] = Request.objects.count()
+        context["pending_requests"] = Request.objects.filter(
+            status=Request.STATUS.PENDING
+        ).count()
+        context["pending_requests_percentage"] = int(
+            context["pending_requests"] * 100 / context["total_requests"]
+        )
+        context["approved_requests"] = Request.objects.filter(
+            status=Request.STATUS.APPROVED
+        ).count()
+        context["approved_requests_percentage"] = int(
+            (context["approved_requests"] * 100) / context["total_requests"]
+        )
+        context["rejected_requests"] = Request.objects.filter(
+            status=Request.STATUS.REJECTED
+        ).count()
+        context["rejected_requests_percentage"] = int(
+            (context["rejected_requests"] * 100) / context["total_requests"]
+        )
         return context
