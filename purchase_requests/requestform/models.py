77a9f876766b8aaa41446ddeab481c609da6e1c2
chriskryx <tanulchristian@live.com>
2022-05-10 17:15:26 +0300

    Create a Manager for Request model, refactor code inside Request, make Request inherit TimeStampedModel
    Implicit querysets inside models.py should make it easier to pass context variables to Views, preventing spaghetti code inside views.py files (i guess).

--- purchase_requests/requestform/models.py
+++ purchase_requests/requestform/models.py
@@ -1,29 +1,64 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.translation import gettext as _
 from model_utils import Choices
 from model_utils.fields import StatusField
 
+from purchase_requests.models import TimeStampedModel
 
-class Request(models.Model):
 
-    STATUS = Choices("APPROVED", "PENDING", "REJECTED")
-    BUDGET_CHOICES = Choices("Project", "Department")
-    TYPE_CHOICES = Choices("Item", "Service")
+class RequestQuerySet(models.QuerySet):
+    def approved(self):
+        return self.filter(status=Request.STATUS.APPROVED)
 
-    creator_name = models.CharField(max_length=150)
-    creator_email = models.EmailField()
+    def pending(self):
+        return self.filter(status=Request.STATUS.PENDING)
+
+    def rejected(self):
+        return self.filter(status=Request.STATUS.REJECTED)
+
+
+class Request(TimeStampedModel):
+    TYPE = Choices(
+        ("ITEM", _("Item")),
+        ("SERVICE", _("Service")),
+        ("WORK_ORDER", _("Work Order")),
+    )
+    BUDGET = Choices(
+        ("PROJECT", _("Project")),
+        ("DEPARTMENT", _("Department")),
+    )
+    STATUS = Choices(
+        ("APPROVED", _("Approved")),
+        ("PENDING", _("Pending")),
+        ("REJECTED", _("Rejected")),
+    )
+
+    status = StatusField(
+        verbose_name="Status", choices=STATUS, default=STATUS.PENDING, db_index=True
+    )
+
+    creator_name = models.CharField(verbose_name="Name", max_length=150)
+    creator_email = models.EmailField(verbose_name="Email")
     type = models.CharField(
-        max_length=7, choices=TYPE_CHOICES, default=TYPE_CHOICES.Item
+        verbose_name="Request Type", max_length=10, choices=TYPE, default=TYPE.ITEM
     )
-    description = models.TextField(max_length=256, blank=True, null=True)
-    reason = models.TextField(max_length=256)
-    est_cost = models.DecimalField(max_digits=6, decimal_places=2)
-    est_delivery = models.DateField()
-    attachment = models.FileField(upload_to="uploads/%Y/%m/%d/", blank=True, null=True)
+    description = models.TextField(
+        verbose_name="Description", max_length=256, blank=True, null=True
+    )
+    reason = models.TextField(verbose_name="Reason", max_length=256)
+    est_cost = models.DecimalField(
+        verbose_name="Estimated Cost", max_digits=6, decimal_places=2
+    )
+    est_delivery = models.DateField(verbose_name="Estimated Delivery Date")
+    attachment = models.FileField(
+        verbose_name="Attachment", upload_to="uploads/%Y/%m/%d/", blank=True
+    )
     budget = models.CharField(
-        max_length=10, choices=BUDGET_CHOICES, blank=True, null=True
+        verbose_name="Budget", max_length=10, choices=BUDGET, blank=True, null=True
     )
-    status = StatusField(choices=STATUS, default=STATUS.PENDING, db_index=True)
+
+    objects = RequestQuerySet.as_manager()
 
     def get_absolute_url(self):
         return reverse("requests:detail", kwargs={"pk": self.pk})
