2c8147119c5e36ae3b30219c9ef1b819a0e1d285
scriptogre <tanulchristian@live.com>
2022-06-02 13:42:30 +0300

    Solve weird interaction which caused Django to throw an error when first running "migrate" with an empty database
    Apparently, when running "python manage.py migrate", the project's urls.py is being checked. Having the "DashboardView" imported in urls.py, it would read over DashboardView and try to solve "Request.objects.all()" query before the tables have been created, thus having Postgres throw an error.

--- purchase_requests/dashboard/views.py
+++ purchase_requests/dashboard/views.py
@@ -7,33 +7,36 @@ from purchase_requests.requestform.models import Request
 class DashboardView(ListView):
     model = Request
     template_name = "dashboard/dashboard.html"
-    extra_context = {
-        "total_count": Request.objects.all().count(),
-        "approved_count": Request.objects.approved().count(),
-        "pending_count": Request.objects.pending().count(),
-        "rejected_count": Request.objects.rejected().count(),
-        "latest_requests": Request.objects.order_by("-id")[:5:-1],
-        "users": get_user_model().objects.all(),
-        "verbose_fields": {
-            field.name: field.verbose_name.title() for field in Request._meta.fields
-        },
-    }
-    extra_context.update(
-        {
-            "approved_percentage": Request.objects.approved().count()
-            * 100
-            / Request.objects.all().count()
-            if Request.objects.all().count()
-            else 0,
-            "pending_percentage": Request.objects.pending().count()
-            * 100
-            / Request.objects.all().count()
-            if Request.objects.all().count()
-            else 0,
-            "rejected_percentage": Request.objects.rejected().count()
-            * 100
-            / Request.objects.all().count()
-            if Request.objects.all().count()
-            else 0,
-        }
-    )
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        context.update(
+            {
+                "total_count": Request.objects.all().count(),
+                "approved_count": Request.objects.approved().count(),
+                "pending_count": Request.objects.pending().count(),
+                "rejected_count": Request.objects.rejected().count(),
+                "latest_requests": Request.objects.order_by("-id")[:5:-1],
+                "users": get_user_model().objects.all(),
+                "verbose_fields": {
+                    field.name: field.verbose_name.title()
+                    for field in Request._meta.fields
+                },
+                "approved_percentage": Request.objects.approved().count()
+                * 100
+                / Request.objects.all().count()
+                if Request.objects.all().count()
+                else 0,
+                "pending_percentage": Request.objects.pending().count()
+                * 100
+                / Request.objects.all().count()
+                if Request.objects.all().count()
+                else 0,
+                "rejected_percentage": Request.objects.rejected().count()
+                * 100
+                / Request.objects.all().count()
+                if Request.objects.all().count()
+                else 0,
+            }
+        )
+        return context
