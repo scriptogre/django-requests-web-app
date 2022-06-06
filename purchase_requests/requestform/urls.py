5a23081a24b8c2a7926dbdf0b8975e07d3e1a085
scriptogre <tanulchristian@live.com>
2022-06-06 19:21:08 +0300

    Create url & template for Request List Grid View
    Also apply some DRY to request_list.html by creation an action_buttons.html template

--- purchase_requests/requestform/urls.py
+++ purchase_requests/requestform/urls.py
@@ -4,14 +4,15 @@ from purchase_requests.requestform.views import (
     RequestCreateView,
     RequestDeleteView,
     RequestDetailView,
+    RequestListGridView,
     RequestListView,
     RequestUpdateView,
 )
 
 app_name = "requestform"
-
 urlpatterns = [
     path("", view=RequestListView.as_view(), name="list"),
+    path("grid/", view=RequestListGridView.as_view(), name="grid_list"),
     path("add/", view=RequestCreateView.as_view(), name="add"),
     path("<int:pk>/", view=RequestDetailView.as_view(), name="detail"),
     path("<int:pk>/update/", view=RequestUpdateView.as_view(), name="update"),
