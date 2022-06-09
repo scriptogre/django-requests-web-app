74eda493acb7c8d1b4bb857c759c4f5287ffebce
scriptogre <tanulchristian@live.com>
2022-06-09 15:33:54 +0300

    Update urls structure, adapt new display_type check to urls.py

--- purchase_requests/requestform/urls.py
+++ purchase_requests/requestform/urls.py
@@ -1,20 +1,29 @@
 from django.urls import path
 
 from purchase_requests.requestform.views import (
+    MyRequestListView,
+    PendingRequestListView,
     RequestCreateView,
     RequestDeleteView,
     RequestDetailView,
-    RequestListGridView,
-    RequestListView,
     RequestUpdateView,
 )
 
 app_name = "requestform"
 urlpatterns = [
-    path("", view=RequestListView.as_view(), name="list"),
-    path("grid/", view=RequestListGridView.as_view(), name="grid_list"),
-    path("add/", view=RequestCreateView.as_view(), name="add"),
-    path("<int:pk>/", view=RequestDetailView.as_view(), name="detail"),
-    path("<int:pk>/update/", view=RequestUpdateView.as_view(), name="update"),
-    path("<int:pk>/delete/", view=RequestDeleteView.as_view(), name="delete"),
+    path("", view=MyRequestListView.as_view(), name="my_requests"),
+    path("my", view=MyRequestListView.as_view(), name="my_requests"),
+    path(
+        "my/<slug:display_type>", view=MyRequestListView.as_view(), name="my_requests"
+    ),
+    path("pending", view=PendingRequestListView.as_view(), name="pending_requests"),
+    path(
+        "pending/<slug:display_type>",
+        view=PendingRequestListView.as_view(),
+        name="pending_requests",
+    ),
+    path("add", view=RequestCreateView.as_view(), name="add"),
+    path("<int:pk>", view=RequestDetailView.as_view(), name="detail"),
+    path("<int:pk>/update", view=RequestUpdateView.as_view(), name="update"),
+    path("<int:pk>/delete", view=RequestDeleteView.as_view(), name="delete"),
 ]
