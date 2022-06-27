65a03077c556983e0d19c7d196ea3eb6098bafe7
scriptogre <tanulchristian@live.com>
2022-06-27 17:41:16 +0300

    Get rid of whitenoise "No directory at" warning in pytest

--- purchase_requisitions/conftest.py
+++ purchase_requisitions/conftest.py
@@ -22,3 +22,16 @@ def user() -> User:
 @pytest.fixture
 def requisition(db) -> Requisition:
     return RequisitionFactory()
+
+
+@pytest.fixture(autouse=True)
+def whitenoise_autorefresh(settings):
+    """
+    Get rid of whitenoise "No directory at" warning, as it's not helpful when running tests.
+
+    Related:
+        - https://github.com/evansd/whitenoise/issues/215
+        - https://github.com/evansd/whitenoise/issues/191
+        - https://github.com/evansd/whitenoise/commit/4204494d44213f7a51229de8bc224cf6d84c01eb
+    """
+    settings.WHITENOISE_AUTOREFRESH = True
